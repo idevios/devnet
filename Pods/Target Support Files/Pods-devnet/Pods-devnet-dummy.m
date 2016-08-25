976524e9aacbddab81f2120fd1b81518325e8099
theerapan <theerapan_kh@ymail.com>
2016-08-25 20:53:05 +0700

    firebase setup

---
+++ Pods/Target Support Files/Pods-devnet/Pods-devnet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_devnet : NSObject
+@end
+@implementation PodsDummy_Pods_devnet
+@end
---
+++ Pods/Target Support Files/Pods-devnet/Pods-devnet-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_devnetVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_devnetVersionString[];
+
