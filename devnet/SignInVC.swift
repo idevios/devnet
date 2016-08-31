7b4c031b1bcb8efa98836805a702241923255abc
theerapan <theerapan_kh@ymail.com>
2016-08-31 22:13:19 +0700

    Added Facebook authentication - objc

--- devnet/SignInVC.swift
+++ devnet/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,42 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonPressed(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("JEDI: Unable to authentication with Facebook: \(error.debugDescription)")
+            } else if result?.isCancelled == true {
+                print("JEDI: User cancel Facebook authentication. ")
+            } else {
+                // IF FACEBOOK AUTH SUCCESS WE WILL BE HERE
+                print("JEDI: Success fully authentication with Facebook. ")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
 
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            
+            if error != nil {
+                print("JEDI: Unable to authentication with Firebase: \(error)")
+            } else {
+                print("JEDI: Successfully authentication with Firebase. ")
+            }
+            
+        })
+        
+    }
+    
 }
+
+
+
+
+
 
