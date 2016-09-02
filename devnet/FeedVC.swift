d9c256007793045521413d8c562b5bfae91b649c
theerapan <theerapan_kh@ymail.com>
2016-09-02 22:22:39 +0700

    COMPLETE Custom TableView

--- devnet/FeedVC.swift
+++ devnet/FeedVC.swift
@@ -10,16 +10,35 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
     @IBAction func signOutButtonPressed(_ sender: AnyObject) {
         let _ = KeychainWrapper.defaultKeychainWrapper().removeObjectForKey(KEY_UID)
         try! FIRAuth.auth()?.signOut()
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostedCell") as! PostedCell
+        return cell
     }
 
 }
---
+++ devnet/PostedCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostedCell.swift
+//  devnet
+//
+//  Created by Theerapan Khanthigul on 9/2/2559 BE.
+//  Copyright © 2559 iDeviOS. All rights reserved.
+//
+
+import UIKit
+
+class PostedCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postedImage: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
