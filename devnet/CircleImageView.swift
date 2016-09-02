48519fa718dc25f086227ab66cab39ee6ee078b5
theerapan <theerapan_kh@ymail.com>
2016-09-02 21:17:34 +0700

    COMPLETE Basic UI, WITHOUT TableViewCustom

---
+++ devnet/CircleImageView.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImageView.swift
+//  devnet
+//
+//  Created by Theerapan Khanthigul on 9/2/2559 BE.
+//  Copyright © 2559 iDeviOS. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.size.width / 2
+    }
+
+}
